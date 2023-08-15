7777cc8b898c7c1118beeab2583b94f8d1e9c9e4
DarckPeanut <134975223+DarckPeanut@users.noreply.github.com>
2023-08-15 19:55:51 +0300

    Add files via upload

--- Eye/index.py
+++ Eye/index.py
@@ -4,11 +4,33 @@ from tkinter import ttk
 root = Tk()     # создаем корневой объект - окно
 root.title("Приложение на Tkinter")     # устанавливаем заголовок окна
 root.geometry("700x450")# устанавливаем размеры окна
+root['bg']=['grey8']
 
-ttk.Style().configure("TLabel",  font="helvetica 13", foreground="#004D40", padding=8, background="#B2DFDB")
 
 
-ttk.Label(text="Hello World!").pack()
-ttk.Label(text="Bye World..").pack()
+
+
+
+
+#Style And Text
+
+ttk.Style().configure("TLabel",  font="helvetica 13", foreground="green", padding=8, background="grey8")
+
+ttk.Label(text="Server to user info").pack(padx=50, pady=20)
+ttk.Label(text="CPU Progressbar").pack(anchor=SE, padx=20)
+
+
+
+
+
+
+#Progress Bars and buttons
+
+value_var = IntVar(value=30)#сноска на значение x переменной загрузки cpu после запроса
+
+ttk.Progressbar(orient='horizontal', length=200, variable=value_var).pack( anchor=SE, padx=20,)
+
+#ttk.Label(text="Hello World!").pack(pady=100, padx=100)
+#ttk.Label(text="Bye World..").pack()
  
 root.mainloop()
