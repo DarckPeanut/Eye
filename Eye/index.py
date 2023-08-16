282ce18b6ac588dab3e15061cdc1764a9f94f0b5
DarckPeanut <134975223+DarckPeanut@users.noreply.github.com>
2023-08-17 02:09:03 +0300

    Add files via upload

--- Eye/index.py
+++ Eye/index.py
@@ -11,13 +11,24 @@ root['bg']=['grey8']
 
 
 
+value_var_1 = IntVar(value=30)  #сноска на значение x переменной загрузки cpu после запроса
+value_var = IntVar(value=45)
 
 #Style And Text
 
 ttk.Style().configure("TLabel",  font="helvetica 13", foreground="green", padding=8, background="grey8")
 
-ttk.Label(text="Server to user info").pack(padx=50, pady=20)
-ttk.Label(text="CPU Progressbar").pack(anchor=SE, padx=20)
+ttk.Label(text="Server to user info").pack(padx=50, pady=20)				#Name
+
+
+ttk.Label(text="CPU Progressbar").pack(anchor=SE, padx=[20, 50], pady=[30,0]) 					#CPU
+ttk.Progressbar(orient='horizontal', length=200, variable=value_var).pack( anchor=SE, padx=20,)
+ttk.Label(text="CPU Progressbar", textvariable=value_var).pack(anchor=SE, padx=[0,180])
+
+
+ttk.Label(text="CPU Progressbar").pack(anchor=SE, padx=[20,50], pady=[50,0]) 					#RAM
+ttk.Progressbar(orient='horizontal', length=200, variable=value_var_1).pack( anchor=SE, padx=20,)
+ttk.Label(text="CPU Progressbar", textvariable=value_var_1).pack(anchor=SE, padx=[0,180])
 
 
 
@@ -26,9 +37,7 @@ ttk.Label(text="CPU Progressbar").pack(anchor=SE, padx=20)
 
 #Progress Bars and buttons
 
-value_var = IntVar(value=30)#сноска на значение x переменной загрузки cpu после запроса
 
-ttk.Progressbar(orient='horizontal', length=200, variable=value_var).pack( anchor=SE, padx=20,)
 
 #ttk.Label(text="Hello World!").pack(pady=100, padx=100)
 #ttk.Label(text="Bye World..").pack()
